871b18dedc9fd4264f19cef7a2b7e02020c0682f
justin-0 <justinitsme1229@gmail.com>
2026-05-09 20:23:04 +0530

    Fix static dirs

--- build.sh
+++ build.sh
@@ -5,5 +5,5 @@ set -o errexit
 python -m pip install --upgrade pip
 pip install -r requirements.txt
 
-python manage.py collectstatic --no-input
+python manage.py collectstatic --no-input --clear
 python manage.py migrate
--- portfolio_project/settings.py
+++ portfolio_project/settings.py
@@ -92,7 +92,8 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 STATIC_ROOT = BASE_DIR / 'staticfiles'
-STATICFILES_DIRS = [BASE_DIR / 'static']
+STATICFILES_DIRS = []
+
 
 # Cloudinary — credentials from environment variables
 import cloudinary
@@ -114,7 +115,7 @@ CLOUDINARY_STORAGE = {
 
 # Media stored on Cloudinary, static files served by WhiteNoise
 DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedStaticFilesStorage'
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
