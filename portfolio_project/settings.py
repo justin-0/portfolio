2e1be5b94d7068df8fe029fca8fe9a00c4be4adb
justin-0 <justinitsme1229@gmail.com>
2026-05-11 11:36:37 +0530

    Fix Cloudinary raw file storage

--- portfolio_project/settings.py
+++ portfolio_project/settings.py
@@ -114,7 +114,7 @@ CLOUDINARY_STORAGE = {
 }
 
 # Media stored on Cloudinary, static files served by WhiteNoise
-DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
+DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.RawMediaCloudinaryStorage'
 STATICFILES_STORAGE = 'django.contrib.staticfiles.storage.StaticFilesStorage'
 
 MEDIA_URL = '/media/'
