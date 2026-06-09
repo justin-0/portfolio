fa39babd244cbd5c86905df5c9b8b42ed5d792a7
justin_james <justinitsme1229@gmail.com>
2026-06-09 12:30:49 +0530

    Update build.sh to include superuser creation
    Add command to create superuser if not exists

--- build.sh
+++ build.sh
@@ -1,10 +1,9 @@
 #!/usr/bin/env bash
 # exit on error
 set -o errexit
-
 python -m pip install --upgrade pip
 pip install -r requirements.txt
-
 mkdir -p staticfiles
 python manage.py collectstatic --no-input --clear
 python manage.py migrate --run-syncdb
+echo "from django.contrib.auth import get_user_model; U = get_user_model(); U.objects.filter(username='admin').exists() or U.objects.create_superuser('admin', 'justion001james@gmail.com', 'admin123')" | python manage.py shell
